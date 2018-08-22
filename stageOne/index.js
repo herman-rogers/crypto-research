26a6d05926ccdcfab4bb3c3eb369d97eeb5c478d
Herman Rogers <herman.rogers@protonmail.com>
2018-08-22 23:21:19 +0200

    Add challenge Three

--- stageOne/index.js
+++ stageOne/index.js
@@ -1,7 +1,18 @@
 const utils = require('../utils/utils');
 
+function decryptXorHash(bytes, xor) {
+  const decrypted = [];
+
+  for(let i = 0; i < bytes.length; i += 1) {
+    const byte = bytes[i];
+
+    decrypted.push(byte ^ xor);
+  }
+  return decrypted;
+}
+
 module.exports = {
-  xorCombination: (hexOne, hexTwo) => {
+  challengeTwoxorCombination: (hexOne, hexTwo) => {
     const hexOneBytes = utils.convertHexToByteArray(hexOne);
     const hexTwoBytes = utils.convertHexToByteArray(hexTwo);
 
@@ -15,5 +26,15 @@ module.exports = {
 
     // Returns a hex combination xor'd from two hex's
     return utils.convertByteArrayToHex(hash);
+  },
+
+  challengeThreexorHexCrack: (hex, characters) => {
+    const hexBytes = utils.convertHexToByteArray(hex);
+    let decrypted = '';
+
+    for(let i = 0; i < characters.length; i += 1) {
+      decrypted = decryptXorHash(hexBytes, characters.charCodeAt(i));
+    }
+    return decrypted;
   },
 };
--- test/stageOne.test.js
+++ test/stageOne.test.js
@@ -1,14 +1,25 @@
 const assert = require('assert');
-const crypto = require('../stageOne');
+const stageOne = require('../stageOne');
 
 describe('stageOne', () => {
-  it('xor combines two hex values', () => {
+  it('xors two hex values together to get a unique hash', () => {
     const hexOne = '1c0111001f010100061a024b53535009181c';
     const hexTwo = '686974207468652062756c6c277320657965';
     const expected = '746865206b696420646f6e277420706c6179';
 
-    const hexCombination = crypto.xorCombination(hexOne, hexTwo);
+    const hexCombination = stageOne.challengeTwoxorCombination(hexOne, hexTwo);
 
     assert.equal(hexCombination, expected);
+  });
+
+  it('can crack a hex value that has be xored', () => {
+    const hex = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736';
+    const cracked = stageOne.challengeThreexorHexCrack(hex, 'X');
+    let crackedString = Buffer.from(cracked).toString();
+
+    crackedString = crackedString.replace(/\0/g, '');
+    crackedString = crackedString.replace(/\07/g, '');
+
+    assert.equal(crackedString, 'Cooking MC\'s like a pound of bacon');
   });
 });
